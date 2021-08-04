46a7b5762d485e7086f80b184c67ff2c2594e7a3
jack <997823131@qq.com>
2021-08-04 17:48:31 +0800

    update

--- src/Upload/Upload.php
+++ src/Upload/Upload.php
@@ -3,7 +3,7 @@
  * 通用文件上传类
  * @author  guanguans <yzmguanguan@gmail.com>
  */
-namespace OssDog\Upload;  // 注意命名空间与 composer.json 中的一致
+namespace OssDog\Sdk\Upload;  // 注意命名空间与 composer.json 中的一致
 
 class Upload
 {
